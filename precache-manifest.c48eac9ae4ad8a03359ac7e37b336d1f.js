34f6f215f7dde054a202cd5493851b82c59be173
José Renato Ferreira Ronca <jrfr21112000@gmail.com>
2020-05-10 14:25:28 -0300

    Updates

---
+++ precache-manifest.c48eac9ae4ad8a03359ac7e37b336d1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32e4d64da3e754ab6234a37903965a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c38ed2e3725e44b350d",
+    "url": "/static/js/2.fd0f66aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd0f66aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078db6614acf18ee5880",
+    "url": "/static/js/main.0ec5639e.chunk.js"
+  },
+  {
+    "revision": "c440c6ec3141bbfba7e2",
+    "url": "/static/js/runtime-main.09a0136b.js"
+  }
+]);
