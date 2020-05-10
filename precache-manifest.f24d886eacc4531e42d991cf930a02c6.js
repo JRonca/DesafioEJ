cda52fa4c68e52596d4e06ab746975f9c391df91
José Renato Ferreira Ronca <jrfr21112000@gmail.com>
2020-05-10 14:29:54 -0300

    Updates

---
+++ precache-manifest.f24d886eacc4531e42d991cf930a02c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1b102124803520563cd1e3438089de",
+    "url": "/DesafioEJ/index.html"
+  },
+  {
+    "revision": "a3671482f506d1e0507c",
+    "url": "/DesafioEJ/static/js/2.0af90040.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DesafioEJ/static/js/2.0af90040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9414376ea0634d41f23e",
+    "url": "/DesafioEJ/static/js/main.3c5acbff.chunk.js"
+  },
+  {
+    "revision": "37f780a82ee948746433",
+    "url": "/DesafioEJ/static/js/runtime-main.4317b7fc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c48eac9ae4ad8a03359ac7e37b336d1f.js"
+  "/DesafioEJ/precache-manifest.f24d886eacc4531e42d991cf930a02c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DesafioEJ/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
